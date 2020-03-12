2506f10dbef19c2538b9b2c950033f656c7f7323
Roney Cacau <roneycacau@gmail.com>
2020-03-12 20:30:22 -0300

    add junit

---
+++ src/aulas/aula3/exercicios/dinossauro/Dinossauro.java
@@ -0,0 +1,63 @@
+package aulas.aula3.exercicios.dinossauro;
+
+public class Dinossauro {
+    int energia;
+    double velocidade;
+    double temperatura;
+    boolean humor;
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public boolean isHumor() {
+        return humor;
+    }
+
+    public void setHumor(boolean humor) {
+        this.humor = humor;
+    }
+
+    public void pular(){}
+
+    public void correr(){}
+
+    public void comer(){}
+
+    public void atirar(){}
+
+    public void tomarSol(){}
+
+    public void ficarNaSombra(){}
+
+    //
+    c
+    private void aumentarVelocidade(){}
+
+    private void aumentarEnergia(){}
+
+    private void mudarHumor(){}
+
+    private void diminuirEnergia(){}
+
+}
